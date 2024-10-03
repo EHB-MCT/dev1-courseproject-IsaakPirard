75629193dd7e217da0fd12f267ef3f3d206eba1a
IsaakPirard <isaak.pirard@student.ehb.be>
2024-10-03 15:26:30 +0200

    Profielfoto getekend

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,57 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawSpaceAlien();
+
+function drawSpaceAlien() {
+	let canvas = document.querySelector("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let context = canvas.getContext("2d");
+
+	context.beginPath();
+	context.rect(100, 100, 300, 300);
+	context.stroke();
+	context.fill();
+
+	context.beginPath();
+	context.rect(200, 100, 50, 50);
+	context.stroke();
+	context.fillStyle = "Green";
+	context.strokeStyle = "Green";
+	context.fill();
+
+	context.beginPath();
+	context.rect(250, 100, 50, 50);
+	context.stroke();
+	context.fillStyle = "Green";
+	context.strokeStyle = "Green";
+	context.fill();
+
+	context.beginPath();
+	context.rect(300, 100, 50, 50);
+	context.stroke();
+	context.fillStyle = "Green";
+	context.strokeStyle = "Green";
+	context.fill();
+
+	context.beginPath();
+	context.rect(200, 150, 50, 50);
+	context.stroke();
+	context.fillStyle = "Green";
+	context.strokeStyle = "Green";
+	context.fill();
+
+	context.beginPath();
+	context.rect(300, 150, 50, 50);
+	context.stroke();
+	context.fillStyle = "Green";
+	context.strokeStyle = "Green";
+	context.fill();
+
+	context.beginPath();
+	context.rect(250, 300, 50, 50);
+	context.stroke();
+	context.fillStyle = "Green";
+	context.strokeStyle = "Green";
+	context.fill();
+}
