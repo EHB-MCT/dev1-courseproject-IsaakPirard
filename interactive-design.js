f4b73891929aad3f098fca6f594d8e13cda8e3ea
IsaakPirard <isaak.pirard@student.ehb.be>
2024-12-19 02:17:17 +0100

    Added tears (animation)

--- interactive-design.js
+++ interactive-design.js
@@ -13,13 +13,28 @@ let width = context.canvas.width;
 let height = context.canvas.height;
 
 let pixelSize = 50;
+let tearMovement = 0;
 
-// ^ Before-code stuff ^
+let randomBoolean;
+let nonRandomBoolean = true;
+let randomValue = randomNumber(1, 2);
 
-drawSadFace(width / 2 - 2 * pixelSize, height / 2);
+if (randomValue % 2 == 0) {
+	randomBoolean = true;
+} else {
+	randomBoolean = false;
+}
 
 function drawPixel(x, y) {
 	context.fillRect(x, y, pixelSize, pixelSize);
+}
+
+// ^ Before-code stuff ^
+
+if (randomBoolean == true) {
+	drawHappyFace(width / 2 - 2 * pixelSize, height / 2);
+} else {
+	drawSadFace(width / 2 - 2 * pixelSize, height / 2);
 }
 
 function drawHappyFace(x, y) {
@@ -70,4 +85,23 @@ function drawSadFace(x, y) {
 	drawPixel(x - 3 * pixelSize, y - 3 * pixelSize);
 	drawPixel(x - 2 * pixelSize, y - 3 * pixelSize);
 	drawPixel(x - 1 * pixelSize, y - 4 * pixelSize);
+
+	if (nonRandomBoolean == true) {
+		drawTears();
+	}
+
+	function drawTears() {
+		tearMovement += 2;
+		context.fillStyle = "Blue";
+		drawPixel(width / 2, height / 2 - 105 + tearMovement);
+		drawPixel(width / 2 - 4 * pixelSize, height / 2 - 105 + tearMovement);
+		requestAnimationFrame(drawTears);
+	}
+}
+
+let mouseDown = false;
+window.onmousedown = verify;
+
+function verify() {
+	mouseDown = true;
 }
